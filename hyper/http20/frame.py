dfbc70bd917d0b81b5d1062b4f6b79305651e8ef
Cory Benfield <lukasaoz@gmail.com>
2014-01-18 18:52:22 +0000

    Build common frame headers.

--- hyper/http20/frame.py
+++ hyper/http20/frame.py
@@ -52,6 +52,27 @@ class Frame(object):
 
         return self.flags
 
+    def build_frame_header(self):
+        # Build the common frame header.
+        length = self._get_len()
+
+        # Get the flags.
+        flags = 0
+
+        for flag, flag_bit in self.defined_flags:
+            if flag in self.flags:
+                flags |= flag_bit
+
+        header = struct.pack(
+            "!HBBL",
+            length & 0x3FFF,  # Length must have the top two bits unset.
+            self.type,
+            flags,
+            self.stream_id & 0x7FFFFFFF  # Stream ID is 32 bits.
+        )
+
+        return header
+
     def serialize(self):
         raise NotImplementedError()
 
