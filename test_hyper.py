0d968c471151daadcb792996fb7e37e8f93ded6c
Cory Benfield <lukasaoz@gmail.com>
2014-01-19 09:57:58 +0000

    Test the RST_STREAM Frame.

--- test_hyper.py
+++ test_hyper.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from hyper.http20.frame import Frame, DataFrame, PriorityFrame
+from hyper.http20.frame import Frame, DataFrame, PriorityFrame, RstStreamFrame
 import pytest
 
 
@@ -44,3 +44,18 @@ class TestPriorityFrame(object):
 
         s = f.serialize()
         assert s == b'\x00\x04\x02\x00\x00\x00\x00\x01\x00\x00\x00\xff'
+
+
+class TestRstStreamFrame(object):
+    def test_rst_stream_frame_has_no_flags(self):
+        f = RstStreamFrame(1)
+        flags = f.parse_flags(0xFF)
+        assert not flags
+        assert isinstance(flags, set)
+
+    def test_rst_stream_frame_serializes_properly(self):
+        f = RstStreamFrame(1)
+        f.error_code = 420
+
+        s = f.serialize()
+        assert s == b'\x00\x04\x03\x00\x00\x00\x00\x01\x00\x00\x01\xa4'
