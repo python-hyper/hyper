71ee54f2347b66343691a7923c933ff0729dd399
Cory Benfield <lukasaoz@gmail.com>
2014-01-29 16:19:48 +0000

    Get all the new fixtures.

--- conftest.py
+++ conftest.py
@@ -3,13 +3,18 @@ import pytest
 import os
 import json
 
+# This pair of generator expressions are pretty lame, but building lists is a
+# bad idea as I plan to have a substantial number of tests here.
+story_directories = (os.path.join('test_fixtures', d) for d in os.listdir('test_fixtures'))
+story_files = (os.path.join(storydir, name) for storydir in story_directories for name in os.listdir(storydir))
+
 @pytest.fixture(scope="class",
-                params=os.listdir('test_fixtures'))
+                params=story_files)
 def story(request):
     """
     Provides a detailed HPACK story to test with.
     """
-    path = os.path.join('test_fixtures', request.param)
+    path = request.param
     with open(path, 'r', encoding='utf-8') as f:
         details = json.loads(f.read())
 
