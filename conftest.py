ed094613535df657c4854dcc677e214a5846e5cb
Cory Benfield <lukasaoz@gmail.com>
2014-01-29 16:35:17 +0000

    Let's pretend I care about PEP8.

--- conftest.py
+++ conftest.py
@@ -5,8 +5,13 @@ import json
 
 # This pair of generator expressions are pretty lame, but building lists is a
 # bad idea as I plan to have a substantial number of tests here.
-story_directories = (os.path.join('test_fixtures', d) for d in os.listdir('test_fixtures'))
-story_files = (os.path.join(storydir, name) for storydir in story_directories for name in os.listdir(storydir))
+story_directories = (
+    os.path.join('test_fixtures', d) for d in os.listdir('test_fixtures')
+)
+story_files = (
+    os.path.join(storydir, name) for storydir in story_directories
+                                 for name in os.listdir(storydir)
+)
 
 @pytest.fixture(scope="class",
                 params=story_files)
