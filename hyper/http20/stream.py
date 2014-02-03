b5289ffc0b830e610eafbd3856bd75e670fa85af
Cory Benfield <lukasaoz@gmail.com>
2014-02-03 18:56:48 +0000

    Define stream objects.

--- hyper/http20/stream.py
+++ hyper/http20/stream.py
@@ -13,3 +13,14 @@ stream is an independent, bi-directional sequence of HTTP headers and data.
 Each stream is identified by a monotonically increasing integer, assigned to
 the stream by the endpoint that initiated the stream.
 """
+class Stream(object):
+    """
+    A single HTTP/2.0 stream.
+
+    A stream is an independent, bi-directional sequence of HTTP headers and
+    data. Each stream is identified by a single integer. From a HTTP
+    perspective, a stream _approximately_ matches a single request-response
+    pair.
+    """
+    def __init__(self, stream_id):
+        self.stream_id = stream_id
