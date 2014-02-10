8768dbc69a0f549c8ad472a9a0ac70db0fbbd4c0
Cory Benfield <lukasaoz@gmail.com>
2014-02-10 19:26:33 +0000

    Easier imports.

--- hyper/__init__.py
+++ hyper/__init__.py
@@ -6,3 +6,5 @@ hyper
 A module for providing an abstraction layer over the differences between
 HTTP/1.1 and HTTP/2.0.
 """
+from .http20.connection import HTTP20Connection
+from .http20.response import HTTP20Response
