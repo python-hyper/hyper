02d9e3b0e8b670b0896db72c9603f6eb70cff9a3
Cory Benfield <lukasaoz@gmail.com>
2014-02-10 20:26:45 +0000

    __version__ and __all__.

--- hyper/__init__.py
+++ hyper/__init__.py
@@ -6,6 +6,8 @@ hyper
 A module for providing an abstraction layer over the differences between
 HTTP/1.1 and HTTP/2.0.
 """
+__version__ = '0.0.1'
+
 from .http20.connection import HTTP20Connection
 from .http20.response import HTTP20Response
 
@@ -13,3 +15,5 @@ from .http20.response import HTTP20Response
 import sys as _sys
 if _sys.version_info[0] < 3 or _sys.version_info[1] < 3:
     raise ImportError("hyper only supports Python 3.3 or higher.")
+
+__all__ = [HTTP20Response, HTTP20Connection]
