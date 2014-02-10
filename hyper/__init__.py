32b87e7c8fec231e66fa2e1ee0b93be1f01976fa
Cory Benfield <lukasaoz@gmail.com>
2014-02-10 20:17:25 +0000

    Py3.3 or higher for now.

--- hyper/__init__.py
+++ hyper/__init__.py
@@ -8,3 +8,8 @@ HTTP/1.1 and HTTP/2.0.
 """
 from .http20.connection import HTTP20Connection
 from .http20.response import HTTP20Response
+
+# Throw import errors on Python 2.
+import sys as _sys
+if _sys.version_info[0] < 3 or _sys.version_info[1] < 3:
+    raise ImportError("hyper only supports Python 3.3 or higher.")
