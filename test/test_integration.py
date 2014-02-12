3ae32a9def74a59d8c23c66f9700eb3765591a60
Cory Benfield <lukasaoz@gmail.com>
2014-02-12 20:53:05 +0000

    First integration test.
    Simple, just to confirm it works.

---
+++ test/test_integration.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+test/integration
+~~~~~~~~~~~~~~~~
+
+This file defines integration-type tests for hyper. These are still not fully
+hitting the network, so that's alright.
+"""
+import ssl
+import threading
+import hyper
+from hyper import HTTP20Connection
+from server import SocketLevelTest
+
+# Turn off certificate verification for the tests.
+hyper.http20.tls._context = hyper.http20.tls._init_context()
+hyper.http20.tls._context.verify_mode = ssl.CERT_NONE
+
+class TestHyperIntegration(SocketLevelTest):
+    def test_connection_string(self):
+        self.set_up()
+
+        # Confirm that we send the connection upgrade string and the initial
+        # SettingsFrame.
+        data = []
+        send_event = threading.Event()
+
+        def socket_handler(listener):
+            sock = listener.accept()[0]
+
+            # We should get two packets: one connection header string, one
+            # SettingsFrame.
+            first = sock.recv(65535)
+            second = sock.recv(65535)
+            data.append(first)
+            data.append(second)
+
+            send_event.set()
+            sock.close()
+
+        self._start_server(socket_handler)
+        conn = HTTP20Connection(self.host, self.port)
+        conn.connect()
+        send_event.wait()
+
+        assert data[0] == b'PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n'
+
+        self.tear_down()
