e237f6a287db819d400d15c5fd71a37643979e0a
Cory Benfield <lukasaoz@gmail.com>
2014-04-20 09:39:00 +0100

    Update Huffman tests.

--- test/test_huffman.py
+++ test/test_huffman.py
@@ -1,22 +1,22 @@
 from hyper.http20.huffman import HuffmanDecoder, HuffmanEncoder
-from hyper.http20.huffman_constants import REQUEST_CODES,REQUEST_CODES_LENGTH,REQUEST_CODES,REQUEST_CODES_LENGTH
+from hyper.http20.huffman_constants import REQUEST_CODES,REQUEST_CODES_LENGTH
 
 
 class TestHuffman(object):
     def test_request_huffman_decoder(self):
         decoder = HuffmanDecoder(REQUEST_CODES,REQUEST_CODES_LENGTH)
-        assert decoder.decode(b'\xdb\x6d\x88\x3e\x68\xd1\xcb\x12\x25\xba\x7f') == b"www.example.com"
-        assert decoder.decode(b'\x63\x65\x4a\x13\x98\xff') == b"no-cache"
-        assert decoder.decode(b'\x4e\xb0\x8b\x74\x97\x90\xfa\x7f') == b"custom-key"
-        assert decoder.decode(b'\x4e\xb0\x8b\x74\x97\x9a\x17\xa8\xff') == b"custom-value"
+        assert decoder.decode(b'\xe7\xcf\x9b\xeb\xe8\x9b\x6f\xb1\x6f\xa9\xb6\xff') == b"www.example.com"
+        assert decoder.decode(b'\xb9\xb9\x94\x95\x56\xbf') == b"no-cache"
+        assert decoder.decode(b'\x57\x1c\x5c\xdb\x73\x7b\x2f\xaf') == b"custom-key"
+        assert decoder.decode(b'\x57\x1c\x5c\xdb\x73\x72\x4d\x9c\x57') == b"custom-value"
 
     def test_request_huffman_encode(self):
         encoder = HuffmanEncoder(REQUEST_CODES, REQUEST_CODES_LENGTH)
-        assert encoder.encode(b"www.example.com") == (b'\xdb\x6d\x88\x3e\x68\xd1\xcb\x12\x25\xba\x7f')
-        assert encoder.encode(b"no-cache") == (b'\x63\x65\x4a\x13\x98\xff')
-        assert encoder.encode(b"custom-key") == (b'\x4e\xb0\x8b\x74\x97\x90\xfa\x7f')
-        assert encoder.encode(b"custom-value") == (b'\x4e\xb0\x8b\x74\x97\x9a\x17\xa8\xff')
+        assert encoder.encode(b"www.example.com") == (b'\xe7\xcf\x9b\xeb\xe8\x9b\x6f\xb1\x6f\xa9\xb6\xff')
+        assert encoder.encode(b"no-cache") == (b'\xb9\xb9\x94\x95\x56\xbf')
+        assert encoder.encode(b"custom-key") == (b'\x57\x1c\x5c\xdb\x73\x7b\x2f\xaf')
+        assert encoder.encode(b"custom-value") == (b'\x57\x1c\x5c\xdb\x73\x72\x4d\x9c\x57')
 
     def test_eos_terminates_decode_request(self):
         decoder = HuffmanDecoder(REQUEST_CODES,REQUEST_CODES_LENGTH)
-        assert decoder.decode(b'\xff\xff\xf7\x00') == b''
+        assert decoder.decode(b'\xff\xff\xee\x00') == b''
