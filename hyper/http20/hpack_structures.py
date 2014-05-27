bdbadd5c9aa81c6ef29704d5f46c93728fc8f49e
Cory Benfield <lukasaoz@gmail.com>
2014-05-27 20:24:37 +0100

    Don't use True/False, keep a count.

--- hyper/http20/hpack_structures.py
+++ hyper/http20/hpack_structures.py
@@ -35,9 +35,9 @@ class Reference(object):
     def __init__(self, obj):
         self.obj = obj
 
-        # Whether the header being referenced by this reference has been
-        # emitted in this round of header processing.
-        self.emitted = False
+        # The number of times the header being referenced by this reference has
+        # been emitted in this round of header processing.
+        self.emitted = 0
 
     def __eq__(self, other):
         return (isinstance(other, Reference) and self.obj is other.obj)
