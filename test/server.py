7d1f3610695cef68d5fb731c7ac249c529cc2401
Cory Benfield <lukasaoz@gmail.com>
2014-06-23 08:33:27 +0100

    Revert "We can remove this hack, at least."
    This reverts commit c6e9a6e0cb86e9e30b4b7649606e8998a4461559
    as part of a reversion of the cipher suites work.

--- test/server.py
+++ test/server.py
@@ -46,6 +46,7 @@ class SocketServerThread(threading.Thread):
             self.cxt.set_npn_protocols([NPN_PROTOCOL])
         self.cxt.load_cert_chain(certfile='test/certs/server.crt',
                                  keyfile='test/certs/server.key')
+        self.cxt.set_ciphers(CIPHER_STRING)
 
     def _start_server(self):
         sock = socket.socket(socket.AF_INET6)
