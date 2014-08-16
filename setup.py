a6a0638a177ac4f56491f2ce306f7c4656e1a7a8
Cory Benfield <lukasaoz@gmail.com>
2014-08-16 11:27:59 +0100

    Remove 2.7, 3.3 and PyPy support from setup.py.

--- setup.py
+++ setup.py
@@ -53,13 +53,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
     ],
     install_requires=resolve_install_requires(),
 )
