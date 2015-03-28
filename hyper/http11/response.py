f13ca275cc172eff7ed79ccecb0674b25d8fe1a2
Cory Benfield <lukasaoz@gmail.com>
2015-03-28 08:47:20 +0000

    Pull out chunked logic from read()

--- hyper/http11/response.py
+++ hyper/http11/response.py
@@ -94,31 +94,8 @@ class HTTP11Response(object):
             return b''
 
         if self._chunked:
-            # If we're doing a full read, read it as chunked and then just join
-            # the chunks together!
-            if amt is None:
-                return self._buffered_data + b''.join(self.read_chunked())
+            return self._normal_read_chunked(amt, decode_content)
 
-            if self._chunker is None:
-                self._chunker = self.read_chunked()
-
-            # Otherwise, we have a certain amount of data we want to read.
-            current_amount = len(self._buffered_data)
-
-            extra_data = [self._buffered_data]
-            while current_amount < amt:
-                try:
-                    chunk = next(self._chunker)
-                except StopIteration:
-                    self.close()
-                    break
-
-                current_amount += len(chunk)
-                extra_data.append(chunk)
-
-            data = b''.join(extra_data)
-            self._buffered_data = data[amt:]
-            return data[:amt]
 
         # If we're asked to do a read without a length, we need to read
         # everything. That means either the entire content length, or until the
@@ -272,6 +249,36 @@ class HTTP11Response(object):
             data += self._decompressobj.flush()
 
         return data
+
+    def _normal_read_chunked(self, amt, decode_content):
+        """
+        Implements the logic for calling ``read()`` on a chunked response.
+        """
+        # If we're doing a full read, read it as chunked and then just join
+        # the chunks together!
+        if amt is None:
+            return self._buffered_data + b''.join(self.read_chunked())
+
+        if self._chunker is None:
+            self._chunker = self.read_chunked()
+
+        # Otherwise, we have a certain amount of data we want to read.
+        current_amount = len(self._buffered_data)
+
+        extra_data = [self._buffered_data]
+        while current_amount < amt:
+            try:
+                chunk = next(self._chunker)
+            except StopIteration:
+                self.close()
+                break
+
+            current_amount += len(chunk)
+            extra_data.append(chunk)
+
+        data = b''.join(extra_data)
+        self._buffered_data = data[amt:]
+        return data[:amt]
 
     # The following methods implement the context manager protocol.
     def __enter__(self):
