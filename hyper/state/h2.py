952079ffd889e092df3ffa58a8ac0fe7bd5d9366
Cory Benfield <lukasaoz@gmail.com>
2015-06-20 18:53:17 +0100

    Further state machine work

--- hyper/state/h2.py
+++ hyper/state/h2.py
@@ -12,6 +12,11 @@ Python HTTP/2 implementation that can be plugged into, for example, Twisted.
 """
 from enum import Enum
 
+from ..packages.hyperframe.frame import (
+    HeadersFrame, ContinuationFrame, DataFrame, WindowUpdateFrame,
+    RstStreamFrame
+)
+
 
 class ProtocolError(Exception):
     """
@@ -33,21 +38,19 @@ class StreamState(Enum):
 class StreamInputs(Enum):
     SEND_HEADERS = 0
     SEND_PUSH_PROMISE = 1
-    SEND_END_HEADERS = 2
-    SEND_RST_STREAM = 3
-    SEND_DATA = 4
-    SEND_WINDOW_UPDATE = 5
-    SEND_END_STREAM = 6
-    RECV_HEADERS = 7
-    RECV_PUSH_PROMISE = 8
-    RECV_END_HEADERS = 9
-    RECV_RST_STREAM = 10
-    RECV_DATA = 11
-    RECV_WINDOW_UPDATE = 12
-    RECV_END_STREAM = 13
+    SEND_RST_STREAM = 2
+    SEND_DATA = 3
+    SEND_WINDOW_UPDATE = 4
+    SEND_END_STREAM = 5
+    RECV_HEADERS = 6
+    RECV_PUSH_PROMISE = 7
+    RECV_RST_STREAM = 8
+    RECV_DATA = 9
+    RECV_WINDOW_UPDATE = 10
+    RECV_END_STREAM = 11
 
 
-class H2Stream(object):
+class H2StreamStateMachine(object):
     """
     A single HTTP/2 stream state machine.
 
@@ -175,3 +178,134 @@ class H2Stream(object):
             self.state = target_state
             if func is not None:
                 return func()
+
+
+class H2Stream(object):
+    """
+    A low-level HTTP/2 stream object. This handles building and receiving
+    frames and maintains per-stream state.
+
+    This wraps a HTTP/2 Stream state machine implementation, ensuring that
+    frames can only be sent/received when the stream is in a valid state.
+    Attempts to create frames that cannot be sent will raise a
+    ``ProtocolError``.
+    """
+    def __init__(self, stream_id):
+        self.state_machine = H2StreamStateMachine(stream_id)
+        self.stream_id = stream_id
+        self.max_outbound_frame_size = None
+        self.max_inbound_frame_size = None
+
+    def send_headers(self, headers, encoder, end_stream=False):
+        """
+        Returns a list of HEADERS/CONTINUATION frames to emit as either headers
+        or trailers.
+        """
+        # Because encoding headers makes an irreversible change to the header
+        # compression context, we make the state transition *first*.
+        self.state_machine.process_input(StreamInputs.SEND_HEADERS)
+        encoded_headers = encoder.encode(headers)
+
+        # Slice into blocks of max_outbound_frame_size. Be careful with this:
+        # it only works right because we never send padded frames or priority
+        # information on the frames. Revisit this if we do.
+        header_blocks = (
+            encoded_headers[i:i+self.max_outbound_frame_size]
+            for i in range(
+                0, len(encoded_headers), self.max_outbound_frame_size
+            )
+        )
+
+        frames = []
+        hf = HeadersFrame(self.stream_id)
+        hf.data = header_blocks[0]
+        frames.append(hf)
+
+        for block in header_blocks[1:]:
+            cf = ContinuationFrame(self.stream_id)
+            cf.data = block
+            frames.append(cf)
+
+        frames[-1].flags.add('END_HEADERS')
+
+        if end_stream:
+            # Not a bug: the END_STREAM flag is valid on the initial HEADERS
+            # frame, not the CONTINUATION frames that follow.
+            self.state_machine.process_input(StreamInputs.END_STREAM)
+            frames[0].flags.add('END_STREAM')
+
+        return frames
+
+    def send_data(self, data, end_stream=False):
+        """
+        Prepare a data frame. Optionally end the stream.
+        """
+        # TODO: Automatically split into multiple frames.
+        # TODO: Something something flow control.
+        self.state_machine.process_input(StreamInputs.SEND_DATA)
+        df = DataFrame(self.stream_id)
+        df.data = data
+
+        if end_stream:
+            df.flags.add('END_STREAM')
+
+        return df
+
+    def end_stream(self):
+        """
+        End a stream without sending data.
+        """
+        self.state_machine.process_input(StreamInputs.END_STREAM)
+        df = DataFrame(self.stream_id)
+        df.flags.add('END_STREAM')
+        return df
+
+    def increase_flow_control_window(self, increment):
+        """
+        Increase the size of the flow control window for the remote side.
+        """
+        self.state_machine.process_input(StreamInputs.SEND_WINDOW_UPDATE)
+        wuf = WindowUpdateFrame(self.stream_id)
+        wuf.window_increment = increment
+        return wuf
+
+    def receive_headers(self, end_stream):
+        """
+        Receive a set of headers (or trailers).
+        """
+        self.state_machine.process_input(StreamInputs.RECV_HEADERS)
+
+        if end_stream:
+            self.state_machine.process_input(StreamInputs.END_STREAM)
+
+    def receive_data(self, end_stream):
+        """
+        Receive some data.
+        """
+        self.state_machine.process_input(StreamInputs.RECV_DATA)
+
+        if end_stream:
+            self.state_machine.process_input(StreamInputs.END_STREAM)
+
+    def receive_window_update(self, increment):
+        """
+        Handle a WINDOW_UPDATE increment.
+        """
+        self.state_machine.process_input(StreamInputs.RECV_WINDOW_UPDATE)
+        # TODO: Actually increment flow control!
+
+    def close(self, error_code=0):
+        """
+        Close the stream locally. Reset the stream with an error code.
+        """
+        self.state_machine.process_input(StreamInputs.SEND_RST_STREAM)
+
+        rsf = RstStreamFrame(self.stream_id)
+        rsf.error_code = error_code
+        return rsf
+
+    def stream_reset(self):
+        """
+        Handle a stream being reset remotely.
+        """
+        self.state_machine.process_input(StreamInputs.RECV_RST_STREAM
