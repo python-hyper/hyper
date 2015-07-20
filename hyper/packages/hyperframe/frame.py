9a6b46c95322562beb0609188afe01f5e3105464
Cory Benfield <lukasaoz@gmail.com>
2015-07-20 18:56:07 +0100

    Update hyperframe to v1.1.1

--- hyper/packages/hyperframe/frame.py
+++ hyper/packages/hyperframe/frame.py
@@ -11,7 +11,7 @@ import collections
 import struct
 
 # The maximum initial length of a frame. Some frames have shorter maximum lengths.
-FRAME_MAX_LEN = (2 ** 14) - 1
+FRAME_MAX_LEN = (2 ** 14)
 
 # The maximum allowed length of a frame.
 FRAME_MAX_ALLOWED_LEN = (2 ** 24) - 1
