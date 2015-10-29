49bb0cceb15fc3a9b088e6e1f20a3c51f38e9661
Cory Benfield <lukasaoz@gmail.com>
2015-10-29 09:47:36 +0900

    Add support for setup.py test.

--- setup.py
+++ setup.py
@@ -5,6 +5,27 @@ import re
 import sys
 
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ['test/']
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 # Get the version
 version_regex = r'__version__ = ["\']([^"\']*)["\']'
@@ -79,6 +100,8 @@ setup(
         'Programming Language :: Python :: Implementation :: CPython',
     ],
     install_requires=resolve_install_requires(),
+    tests_require=['pytest', 'requests', 'mock'],
+    cmdclass={'test': PyTest},
     entry_points={
         'console_scripts': [
             'hyper = hyper.cli:main',
