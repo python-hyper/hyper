b2d2b7fe29766e2410e291a1fa07ce4af75f1a7f
Ben Last <ben@benlast.com>
2015-12-23 09:42:02 -0800

    Use `pytest.skip`

--- test/test_hyper_SSLContext.py
+++ test/test_hyper_SSLContext.py
@@ -4,6 +4,8 @@ Tests the hyper SSLContext.
 """
 import os
 
+import pytest
+
 from hyper.compat import ssl
 try:
     from hyper.ssl_compat import SSLContext
@@ -21,17 +23,21 @@ class TestHyperSSLContext(object):
     """
     Tests hyper SSLContext
     """
+
+    @pytest.mark.skipif(
+        SSLContext is None,
+        reason='requires PyOpenSSL'
+    )
     def test_custom_context_with_cert_as_file(self):
         # Test using hyper's own SSLContext
-        if SSLContext is not None:
-            context = SSLContext(ssl.PROTOCOL_SSLv23)
-            context.verify_mode = ssl.CERT_NONE
-            context.check_hostname = False
+        context = SSLContext(ssl.PROTOCOL_SSLv23)
+        context.verify_mode = ssl.CERT_NONE
+        context.check_hostname = False
 
-            # Test that we can load in a cert and key protected by a passphrase,
-            # from files.
-            context.load_cert_chain(
-                certfile=CLIENT_CERT_FILE,
-                keyfile=CLIENT_KEY_FILE,
-                password='abc123'
-            )
+        # Test that we can load in a cert and key protected by a passphrase,
+        # from files.
+        context.load_cert_chain(
+            certfile=CLIENT_CERT_FILE,
+            keyfile=CLIENT_KEY_FILE,
+            password='abc123'
+        )
