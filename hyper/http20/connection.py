190e89462ae810cbcaabb87eb305ae2c6b4a163c
Eugene Obukhov <irvind25@gmail.com>
2015-12-25 15:40:23 +0300

    Fix stream creation

--- hyper/http20/connection.py
+++ hyper/http20/connection.py
@@ -507,10 +507,11 @@ class HTTP20Connection(object):
         """
         Returns a new stream object for this connection.
         """
+        window_size = self._settings[SettingsFrame.INITIAL_WINDOW_SIZE]
         s = Stream(
             stream_id or self.next_stream_id, self._send_cb, self._recv_cb,
             self._close_stream, self.encoder, self.decoder,
-            self.__wm_class(65535), local_closed
+            self.__wm_class(window_size), local_closed
         )
         s._out_flow_control_window = self._out_flow_control_window
         self.streams[s.stream_id] = s
