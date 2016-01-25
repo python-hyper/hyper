22afa360060e867ab1279fc8e2e8a992c6881afc
Cory Benfield <lukasaoz@gmail.com>
2016-01-25 14:32:45 +0000

    Add hyper-h2 to dependencies

--- setup.py
+++ setup.py
@@ -53,17 +53,22 @@ except AttributeError:
 
 
 def resolve_install_requires():
+    basic_dependencies = ['h2~=2.0']
+
     if py_version == (3, 3):
-        return ['pyOpenSSL>=0.15', 'service_identity>=14.0.0']
+        basic_dependencies.extend(
+            ['pyOpenSSL>=0.15', 'service_identity>=14.0.0']
+        )
     elif py_version == (2, 7) and py_long_version < (2, 7, 9):
-        deps = ['pyOpenSSL>=0.15', 'service_identity>=14.0.0']
+        basic_dependencies.extend(
+            ['pyOpenSSL>=0.15', 'service_identity>=14.0.0']
+        )
 
         # PyPy earlier than 2.6.0 doesn't support cryptography 1.0
         if pypy_version and pypy_version < (2, 6):
-            deps.append('cryptography<1.0')
+            basic_dependencies.append('cryptography<1.0')
 
-        return deps
-    return []
+    return basic_dependencies
 
 packages = [
     'hyper',
