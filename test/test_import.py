30413013b09b90de63b3bd6e375ae47b0d63ec1a
Cory Benfield <lukasaoz@gmail.com>
2016-12-02 11:12:39 +0000

    Skip import tests on wrong platforms.

--- test/test_import.py
+++ test/test_import.py
@@ -5,6 +5,7 @@ import sys
 
 
 class TestImportPython2(object):
+    @pytest.mark.skipif(sys.version_info[0] == 3, reason="Python 2 only")
     def test_cannot_import_python_2(self, monkeypatch):
         monkeypatch.setattr(sys, 'version_info', (2, 6, 5, 'final', 0))
         with pytest.raises(ImportError):
@@ -12,6 +13,7 @@ class TestImportPython2(object):
 
 
 class TestImportPython3(object):
+    @pytest.mark.skipif(sys.version_info[0] == 2, reason="Python 3 only")
     def test_cannot_import_python_32(self, monkeypatch):
         monkeypatch.setattr(sys, 'version_info', (3, 2, 3, 'final', 0))
         with pytest.raises(ImportError):
