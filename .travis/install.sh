3046d73bca400481f0cf4ba5406eece50304d79b
KOLANICH <kolan_n@mail.ru>
2021-01-12 16:44:56 +0300

    Moved the metadata into setup.cfg. Added pyproject.toml.

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,7 +7,9 @@ if [[ "$HYPER_FAST_PARSE" = true ]]; then
     pip install pycohttpparser~=1.0
 fi
 
-pip install -U setuptools
-pip install .
+pip install -U pip
+pip install -U setuptools wheel build
+python3 -m build -nwx .
+pip install --upgrade ./dist/*.whl
 pip install -r test_requirements.txt
 pip install flake8
